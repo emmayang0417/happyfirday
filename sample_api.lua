a8fcacde3ff561cf5408a1f3c2d952c0af796e69
Gene Cheng <genecheng@exosite.com>
2016-11-04 10:48:49 +0800

    add new endpoint

--- sample_api.lua
+++ sample_api.lua
@@ -1,3 +1,26 @@
+--#ENDPOINT GET /development/device/moredata
+-- Description: Get timeseries data for specific device
+-- Parameters: ?identifier=<uniqueidentifier>&limit=<number>
+local identifier = tostring(request.parameters.identifier) -- ?identifier=<uniqueidentifier>
+local limit = tostring(request.parameters.limit) -- in minutes,if ?window=<number>
+if true then
+  local data = {}
+  if limit == nil then limit = '30' end
+    local q = TSQ.q()
+
+      :fields('value')
+      :from('speed','rpm','gps','maf','engine_fuel_rate','throttle_pos',
+            'acc_pedal_pos_d','amb_temp')
+      :limit(tonumber(limit))
+                :orderby()
+
+    local response = Timeseries.query{q = tostring(q) }
+  data['timeseries'] = response
+  return data
+else
+  http_error(403, response)
+end
+
 --#ENDPOINT GET /development/device/data
 -- Description: Get timeseries data for specific device
 -- Parameters: ?identifier=<uniqueidentifier>&limit=<number>
